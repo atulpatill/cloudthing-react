937e35c1a4db393aa0efc5212abb4613f7c6a99f
atul patil <atulpatil539@gmail.com>
2022-06-01 21:02:07 +0530

    feat : updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,106 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from "styled-components";
+import axios from 'axios';
 import './App.css';
+import NewsComponent from './Components/NewsComponent';
+
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+`;
+
+const Header = styled.div`
+display :flex;
+flex-direction: row;
+background-color: black;
+justify-content: space-between;
+align-items: center;
+color:white;
+padding: 10px;
+font-size: 25px;
+font-weight: bold;
+box-shadow: 0 3px 6px 0 #555;
+`;
+const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  border-radius: 6px;
+  margin-left: 20px;
+  width: 50%;
+  background-color: white;
+`;
+const SearchIcon = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+`;
 
 function App() {
+
+  const [searchQuery, updateSearchQuery] = useState();
+  const [timeoutId, updateTimeoutId] = useState();
+  const [newsList, updateNewsList] = useState([])
+  
+
+  const fetchData = async (searchString) => {
+    const Response = await axios.get(
+        ` http://content.guardianapis.com/search?api-key=test&q=${searchString}&show- fields=thumbnail,headline&page=1&page-size=10`);
+        
+        updateNewsList(Response.data.response.results);
+        console.log(Response.data.response.results)
+    };
+
+  const onTextChange = (event) => {
+    clearTimeout(timeoutId)
+   updateSearchQuery(event.target.value)
+   const timeout = setTimeout(() => fetchData(event.target.value), 500);
+   updateTimeoutId(timeout);
+  };
+
+  const NewsListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 30px;
+  gap: 25px;
+  justify-content: space-evenly;;
+`;
+
   return (
-    <div className="App">
-     <h1>CloudThing</h1>
-    </div>
+    <Container>
+      
+     <Header>
+       React News App
+       <SearchBox>
+        <SearchIcon src="/search-icon.svg"/><SearchInput 
+          placeholder="Search News"
+          value={searchQuery}
+            onChange={onTextChange} 
+            />
+        </SearchBox>
+
+     </Header>
+     <NewsListContainer>
+     {newsList?.length 
+          ? newsList.map((news, id) => <NewsComponent   
+            key={id}
+          news={news} 
+        
+          />)
+          : "No News to Display" }
+     </NewsListContainer>
+
+
+    </Container>
   );
 }
 
---
+++ src/Components/NewsComponent.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import styled from "styled-components";
+
+const MovieContainer = styled.div`
+display: flex;
+flex-direction: column;
+padding: 10px;
+width: 280px;
+box-shadow: 0 3px 10px 0 #aaa;
+cursor: pointer;
+`;
+
+// ..............
+
+const CoverImage = styled.img`
+  object-fit: cover;
+  height: 362px;
+`;
+const NewsName = styled.span`
+  font-size: 18px;
+  font-weight: 600;
+  color: black;
+  margin: 15px 0;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+const InfoColumn = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const NewsInfo = styled.span`
+  font-size: 16px;
+  font-weight: 500;
+  color: black;
+  white-space: nowrap;
+  overflow: hidden;
+  text-transform: capitalize;
+  text-overflow: ellipsis;
+`;
+
+
+function NewsComponent(props) {
+    const {type, webTitle, webUrl, sectionName}  = props.news
+   
+  return (
+    <MovieContainer onClick={() => window.open(webUrl, "_blank")}>
+        <CoverImage src={webUrl} alt="Deafult" />
+      <NewsName>{webTitle}</NewsName>
+      <InfoColumn>
+        <NewsInfo>Section : {sectionName}</NewsInfo>
+        <NewsInfo>Type :{type} </NewsInfo>
+      </InfoColumn>
+    </MovieContainer>
+  )
+}
+
+export default NewsComponent
